ded539eeefdf2951718d070c9d05cbd904fa5bbd
juhyunju <2juhyunju@naver.com>
2023-11-08 15:04:27 +0900

    a

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const routes = require('./routes');
+dotenv.config()
 
 const app = express()
 app.use(cors())
