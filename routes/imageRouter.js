350d88458a60129b2d9c66b3bfa66f7b53c09041
juhyunju <2juhyunju@naver.com>
2023-11-13 16:37:08 +0900

    feat: add auth

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router()
 const imageController = require('../controller/imageController')
 const upload = require("../middleware/upload")
+const auth = require('../middleware/auth')
 
-router.post('/', upload.single('image'), imageController.uploadImage);
+router.post('/',auth.hostVerifyToken, upload.single('image'), imageController.uploadImage);
 router.get('/',imageController.getImages)
 router.get('/:classId',imageController.getImage)
-router.delete('/:id',imageController.deleteImage)
+router.delete('/:id',auth.hostVerifyToken,imageController.deleteImage)
 
 
 module.exports = router
