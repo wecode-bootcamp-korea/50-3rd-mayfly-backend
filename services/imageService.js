64b7a2c6d5cd5dd6678931cf8536c9b2b29dd340
juhyunju <2juhyunju@naver.com>
2023-11-17 15:15:45 +0900

    feat:PR

--- services/imageService.js
+++ services/imageService.js
@@ -1,10 +1,6 @@
 const imageDao = require('../models/imageDao');
 const { error } = require("../middleware/error");
 
-// const saveImage = async (imageUrl) => {
-//     const image = await imageDao.createImages(imageUrl);
-//     return image
-// };
 const getImages = async()=>{
   const images = await imageDao.getImages()
   if(images.length === 0){
@@ -26,7 +22,6 @@ const deleteImage = async(id) => {
 }
 
 module.exports = {
-  // saveImage,
   getImages,
   getImage,
   deleteImage
